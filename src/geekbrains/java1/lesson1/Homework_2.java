2c1f0c4fc553a7d11bc1939fa7c3ec4532ceb045
Shaligin <redgy1@yandex.ru>
2020-04-08 19:15:03 +0300

    homework 2 in procces

--- src/geekbrains/java1/lesson1/Homework_2.java
+++ src/geekbrains/java1/lesson1/Homework_2.java
@@ -1,7 +1,22 @@
 package geekbrains.java1.lesson1;
 
-public class Homework_2 {
-    private static void main (String[]args){
+import java.util.Arrays;
 
+public class Homework_2 {
+    public static void main(String[] args) {
+        int[] jony = {1, 0, 1, 0, 0,1, 1};
+        System.out.print (EVA(jony));
+    }
+
+    public static String EVA(int[] jony) {
+
+        for (int i = 0; i < jony.length; i++) {
+            if (jony[i] != 0) {
+                jony[i] = 0;
+            } else {
+                jony[i] = 1;
+            }
+        }
+        return (Arrays.toString(jony));
     }
 }
