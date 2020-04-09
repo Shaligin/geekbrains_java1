b48f305a1fb21ff0af0fe24f17fe11b36d38a8e7
Shaligin <redgy1@yandex.ru>
2020-04-09 10:20:32 +0300

    homework 2 in procces

--- src/geekbrains/java1/lesson1/Homework_1.java
+++ src/geekbrains/java1/lesson1/Homework_1.java
@@ -1,4 +1,42 @@
 package geekbrains.java1.lesson1;
 
 public class Homework_1 {
+    public static void main(String[] args) {
+//        int q = 10;
+//        int w = 2;
+//        int e = 3;
+//        int r = 1;
+//        System.out.println(EVA (q, w, e, r));
+//        System.out.print (SARA (5, 16));
+        NIKA(56);
+        System.out.println(KAT("Aleks"));
+
+    }
+
+    private static float EVA(int a, int b, int c, int d) {
+        return a * (b + (c * 1f / d));
+    }
+
+    private static boolean SARA(int a, int b) {
+        int sum = a + b; //this code is Ivan-code.
+        //return (a + b >= 10 && a + b <= 20);
+        return (sum >= 10 && sum <= 20);
+    }
+
+    private static void NIKA(int x) {
+        if (x >= 0) {
+            System.out.println("Number is positive");
+        } else {
+            System.out.println("Number is negative");
+        }
+        //return(); //если я захочу вернуть то println нужно будет печатать при вызове метода
+    }
+
+    private static String KAT(String name) {
+        return "Hello, " + name;
+    }
+
 }
+
+
+
--- src/geekbrains/java1/lesson1/Homework_2.java
+++ src/geekbrains/java1/lesson1/Homework_2.java
@@ -4,19 +4,35 @@ import java.util.Arrays;
 
 public class Homework_2 {
     public static void main(String[] args) {
-        int[] jony = {1, 0, 1, 0, 0,1, 1};
-        System.out.print (EVA(jony));
+        int jony[] = {1, 0, 1, 0, 0, 1, 1};
+        //EVA(jony);
+        System.out.println(EVA(jony));  //в задание не сказанно возвратить в консоль, но мне хотелось посмотреть работу метода
+        //NIKA();
+        //System.out.println(NIKA());
     }
 
-    public static String EVA(int[] jony) {
 
+    public static String EVA(int[] jony) {
         for (int i = 0; i < jony.length; i++) {
-            if (jony[i] != 0) {
-                jony[i] = 0;
-            } else {
-                jony[i] = 1;
-            }
+            jony[i] = jony[i] != 0 ? 0 : 1;
+
+//            if (jony[i] != 0) {
+//                jony[i] = 0;
+//            } else {
+//                jony[i] = 1;
+//            }
         }
         return (Arrays.toString(jony));
     }
+
+
+//    public static String NIKA() {
+//        int[] sam = new int[8];
+//        for (int i = 0; i < sam.length; i++) {
+//            sam[i] = (i * 3) + 1;
+//        }
+//        return (Arrays.toString(sam));
+//    }
+
+
 }
